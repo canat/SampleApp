6a125e6c2b8b45776d32b06c7481527dea0cf966
Davut CAVDAR <davutcavdar@gmail.com>
2018-02-27 12:43:35 +0300

    cast to IPCameraApplicationAdapter

--- src/main/java/io/antmedia/serverapp/pscp/Application.java
+++ src/main/java/io/antmedia/serverapp/pscp/Application.java
@@ -1,36 +1,18 @@
 package io.antmedia.serverapp.pscp;
 
-
-import java.io.File;
-import java.util.List;
-
-import org.red5.server.adapter.MultiThreadedApplicationAdapter;
 import org.red5.server.api.IConnection;
-import org.red5.server.api.scheduling.IScheduledJob;
-import org.red5.server.api.scheduling.ISchedulingService;
 import org.red5.server.api.scope.IScope;
-//import org.slf4j.Logger;
-import org.red5.server.api.stream.IBroadcastStream;
-import org.red5.server.api.stream.IStreamListener;
-import org.red5.server.api.stream.IStreamPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-
-import io.antmedia.AntMediaApplicationAdapter;
-import io.antmedia.datastore.db.IDataStore;
-import io.antmedia.muxer.IMuxerListener;
-import io.antmedia.muxer.MuxAdaptor;
-import io.antmedia.social.endpoint.VideoServiceEndpoint;
-import io.antmedia.social.endpoint.VideoServiceEndpoint.DeviceAuthParameters;
+import io.antmedia.ipcamera.IPCameraApplicationAdapter;
 
 /**
  * Sample application that uses the client manager.
  * 
  * @author The Red5 Project (red5@osflash.org)
  */
-public class Application extends AntMediaApplicationAdapter {
+public class Application extends IPCameraApplicationAdapter {
 
 	protected static Logger logger = LoggerFactory.getLogger(Application.class);
 
@@ -54,5 +36,3 @@ public class Application extends AntMediaApplicationAdapter {
 	}
 
 }
-
-
