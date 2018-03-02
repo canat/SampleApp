fb5ed5183c2bac251bd7d2dc394229f9aee70903
mekya <ahmetmermerkaya@gmail.com>
2018-03-02 19:30:12 +0300

    Enable fullscreen in flowplayer, fix IE11 issue

--- src/main/webapp/js/fetch.stream.js
+++ src/main/webapp/js/fetch.stream.js
@@ -1,4 +1,19 @@
 //ask if adaptive m3u8 file
+
+if (!String.prototype.endsWith) 
+{
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.lastIndexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
+
 function tryToPlay(name) 
 {
 	fetch("streams/"+ name +"_adaptive.m3u8", {method:'HEAD'})
